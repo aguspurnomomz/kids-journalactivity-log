7e9ddbae377d635c1323253be63cb07407f6c683
AgusPurnomo <agus.purnomomz@gmail.com>
2026-08-12 10:39:39 +0700

    feat: change firebase-messaging js folder to public

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,7 +19,7 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/favicon.ico'
+    icon: '/logo.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
